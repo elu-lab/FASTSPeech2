5f6484efa121b7d65a6a766ae3130b5dd8bff4d9
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-08-17 23:50:13 +0900

    Update tools.py
    get_mask_from_lengths function Added

--- utils/tools.py
+++ utils/tools.py
@@ -13,6 +13,18 @@ from matplotlib import pyplot as plt
 matplotlib.use("Agg")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
+############################# @ transformer #################################
+def get_mask_from_lengths(lengths, max_len=None):
+    batch_size = lengths.shape[0]
+    if max_len is None:
+        max_len = torch.max(lengths).item()
+
+    ids = torch.arange(0, max_len).unsqueeze(0).expand(batch_size, -1).to(device)
+    mask = ids >= lengths.unsqueeze(1).expand(-1, max_len)
+
+    return mask
+
 #################### @ dataset.py - Dataset ################
 def pad_1D(inputs, PAD=0):
 
