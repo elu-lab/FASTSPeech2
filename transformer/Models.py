dcb172a684bfbcbb379db7c19fd5ca151d54958a
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-08-17 23:47:53 +0900

    Create Models.py
    Models.py; Positional Encoding function only

---
+++ transformer/Models.py
@@ -0,0 +1,37 @@
+
+#### Github [transformer.Models.py]: https://github.com/ming024/FastSpeech2/blob/master/transformer/Models.py
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+import transformer.Constants as Constants
+# from .Layers import FFTBlock ## Not yet
+from text.symbols import symbols
+
+############################# @ transformer #################################
+def get_sinusoid_encoding_table(n_position, d_hid, padding_idx=None):
+    """ Sinusoid position encoding table """
+
+    def cal_angle(position, hid_idx):
+        return position / np.power(10000, 2 * (hid_idx // 2) / d_hid)
+
+    def get_posi_angle_vec(position):
+        return [cal_angle(position, hid_j) for hid_j in range(d_hid)]
+
+    sinusoid_table = np.array(
+        [get_posi_angle_vec(pos_i) for pos_i in range(n_position)]
+    )
+
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
+
+    if padding_idx is not None:
+        # zero vector for padding dimension
+        sinusoid_table[padding_idx] = 0.0
+
+    return torch.FloatTensor(sinusoid_table)
+
+
+
+
