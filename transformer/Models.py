bca20e864eff31183aa3cf27ae1940bbfff82326
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-08-18 16:40:08 +0900

    Update Models.py
    Added Encoder

--- transformer/Models.py
+++ transformer/Models.py
@@ -1,12 +1,13 @@
 
 #### Github [transformer.Models.py]: https://github.com/ming024/FastSpeech2/blob/master/transformer/Models.py
-# changed 'import numpy' as np position
+
+
 import numpy as np
 import torch
 import torch.nn as nn
 
 import transformer.Constants as Constants
-# from .Layers import FFTBlock ## Not yet
+from .Layers import FFTBlock ## Not yet
 from text.symbols import symbols
 
 ############################# @ transformer #################################
@@ -31,6 +32,61 @@ def get_sinusoid_encoding_table(n_position, d_hid, padding_idx=None):
         sinusoid_table[padding_idx] = 0.0
 
     return torch.FloatTensor(sinusoid_table)
+
+
+
+############################# @ fastspeech2 #################################
+class Encoder(nn.Module):
+    """ Encoder """
+
+    def __init__(self, config=None):
+        super(Encoder, self).__init__()
+
+        n_position = config["max_seq_len"] + 1 # 1000 + 1 
+        n_src_vocab = len(symbols) + 1 # 360 + 1 
+        d_word_vec = config["transformer"]["encoder_hidden"] # 256 
+        n_layers = config["transformer"]["encoder_layer"] # 4 
+        n_head = config["transformer"]["encoder_head"] # 2 
+        d_k = d_v = (config["transformer"]["encoder_hidden"] // config["transformer"]["encoder_head"]) # 128  
+        d_model = config["transformer"]["encoder_hidden"]# 256 
+        d_inner = config["transformer"]["conv_filter_size"] # 1024 
+        kernel_size = config["transformer"]["conv_kernel_size"] # [9, 1] 
+        dropout = config["transformer"]["encoder_dropout"] # .1 
+
+        self.max_seq_len = config["max_seq_len"] # 1000 
+        self.d_model = d_model
+
+        self.src_word_emb = nn.Embedding(n_src_vocab, d_word_vec, padding_idx= Constants.PAD)
+        self.position_enc = nn.Parameter(get_sinusoid_encoding_table(n_position, d_word_vec).unsqueeze(0), requires_grad=False,)
+
+        self.layer_stack = nn.ModuleList([FFTBlock(d_model, n_head, d_k, d_v, d_inner, kernel_size, dropout=dropout) for _ in range(n_layers)])
+
+    def forward(self, src_seq, mask, return_attns=False):
+        ### src_seq = texts
+        ### mask: src_masks
+
+        enc_slf_attn_list = []
+        batch_size, max_len = src_seq.shape[0], src_seq.shape[1]
+
+        # -- Prepare masks
+        slf_attn_mask = mask.unsqueeze(1).expand(-1, max_len, -1)
+
+        # -- Forward
+        ### Don't know here: self.training??
+        if not self.training and src_seq.shape[1] > self.max_seq_len:
+            # print("Upper"): not printed
+            enc_output = self.src_word_emb(src_seq) + get_sinusoid_encoding_table(src_seq.shape[1], self.d_model)[: src_seq.shape[1], :].unsqueeze(0).expand(batch_size, -1, -1).to(src_seq.device)
+        else:
+            #### BASED on `else`
+            # print("Bottom"): printed
+            enc_output = self.src_word_emb(src_seq) + self.position_enc[:, :max_len, :].expand(batch_size, -1, -1)
+
+        for enc_layer in self.layer_stack:
+            enc_output, enc_slf_attn = enc_layer(enc_output, mask=mask, slf_attn_mask=slf_attn_mask )
+            if return_attns:
+                enc_slf_attn_list += [enc_slf_attn]
+
+        return enc_output
 
 
 
