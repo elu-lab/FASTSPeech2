22f5d27b3381223bf8817097f614323c42c65b97
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-08-21 21:17:21 +0900

    Update symbols.py
    Applied 'added' pinyin for german

--- text/symbols.py
+++ text/symbols.py
@@ -16,6 +16,7 @@ _silences = ["@sp", "@spn", "@sil"]
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ["@" + s for s in cmudict.valid_symbols]
 _pinyin = ["@" + s for s in pinyin.valid_symbols]
+_pinyin2 = [s for s in pinyin.valid_symbols2]
 
 # Export all symbols:
 symbols = (
@@ -25,5 +26,6 @@ symbols = (
     + list(_letters)
     + _arpabet
     + _pinyin
+    + _pinyin2
     + _silences
 )
