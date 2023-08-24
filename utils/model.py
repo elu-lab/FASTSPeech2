afd7c864e5d5f2288596cfd9b5794e0b2f95791c
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-08-24 21:12:48 +0900

    Update model.py
    modified get_model and get_vocoder bcz of MelGAN doesnt work

--- utils/model.py
+++ utils/model.py
@@ -12,14 +12,15 @@ def get_model(args, configs, device, train=False):
     (preprocess_config, model_config, train_config) = configs
 
     model = FastSpeech2(preprocess_config, model_config).to(device)
-    if args.restore_step:
+    if args is not None and args.restore_step:
         ckpt_path = os.path.join(train_config["path"]["ckpt_path"], "{}.pth.tar".format(args.restore_step),)
         ckpt = torch.load(ckpt_path)
         model.load_state_dict(ckpt["model"])
 
     if train:
-        scheduled_optim = ScheduledOptim( model, train_config, model_config, args.restore_step)
-        if args.restore_step:
+        scheduled_optim = ScheduledOptim( model, train_config, model_config, 0)
+        if args is not None and args.restore_step:
+            scheduled_optim = ScheduledOptim( model, train_config, model_config, args.restore_step)
             scheduled_optim.load_state_dict(ckpt["optimizer"])
         model.train()
         return model, scheduled_optim
@@ -27,6 +28,34 @@ def get_model(args, configs, device, train=False):
     model.eval()
     model.requires_grad_ = False
     return model
+
+
+################################# Original #####################################
+# def get_model(args, configs, device, train=False):
+#     (preprocess_config, model_config, train_config) = configs
+
+#     model = FastSpeech2(preprocess_config, model_config).to(device)
+#     if args.restore_step:
+#         ckpt_path = os.path.join(
+#             train_config["path"]["ckpt_path"],
+#             "{}.pth.tar".format(args.restore_step),
+#         )
+#         ckpt = torch.load(ckpt_path)
+#         model.load_state_dict(ckpt["model"])
+
+#     if train:
+#         scheduled_optim = ScheduledOptim(
+#             model, train_config, model_config, args.restore_step
+#         )
+#         if args.restore_step:
+#             scheduled_optim.load_state_dict(ckpt["optimizer"])
+#         model.train()
+#         return model, scheduled_optim
+
+#     model.eval()
+#     model.requires_grad_ = False
+#     return model
+
 
 
 def get_param_num(model):
@@ -43,22 +72,27 @@ def get_vocoder(config, device):
         if speaker == "LJSpeech":
             vocoder = torch.hub.load("descriptinc/melgan-neurips", "load_melgan", "linda_johnson" )
         elif speaker == "universal":
-            vocoder = torch.hub.load( "descriptinc/melgan-neurips", "load_melgan", "multi_speaker" )
+            vocoder = torch.hub.load( "descriptinc/melgan-neurips", "load_melgan", "multi_speaker" ) ## Doesn't work
+            ### mel() takes 0 positional arguments but 5 were given
         vocoder.mel2wav.eval()
         vocoder.mel2wav.to(device)
-    # elif name == "HiFi-GAN":
-    #     with open("hifigan/config.json", "r") as f:
-    #         config = json.load(f)
-    #     config = hifigan.AttrDict(config)
-    #     vocoder = hifigan.Generator(config)
-    #     if speaker == "LJSpeech":
-    #         ckpt = torch.load("hifigan/generator_LJSpeech.pth.tar")
-    #     elif speaker == "universal":
-    #         ckpt = torch.load("hifigan/generator_universal.pth.tar")
-    #     vocoder.load_state_dict(ckpt["generator"])
-    #     vocoder.eval()
-    #     vocoder.remove_weight_norm()
-    #     vocoder.to(device)
+    elif name == "HiFi-GAN":
+        # with open("hifigan/config.json", "r") as f:
+        #     config = json.load(f)
+        # config = hifigan.AttrDict(config)
+        # vocoder = hifigan.Generator(config)
+        # if speaker == "LJSpeech":
+        #     ckpt = torch.load("hifigan/generator_LJSpeech.pth.tar")
+        # elif speaker == "universal":
+        #     ckpt = torch.load("hifigan/generator_universal.pth.tar")
+        # vocoder.load_state_dict(ckpt["generator"])
+
+        # hifigan, vocoder_train_setup, denoiser = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_hifigan') ## Worked
+        vocoder, vocoder_train_setup, denoiser = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_hifigan') ## Worked
+        
+        vocoder.eval()
+        # vocoder.remove_weight_norm()
+        vocoder.to(device)
 
     return vocoder
 
