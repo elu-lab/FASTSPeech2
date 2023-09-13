71e85331b877cd641603ba06241d6e0fa6d5af0d
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-09-13 09:21:25 +0900

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -19,8 +19,42 @@ _silences = ["@sp", "@spn", "@sil"]
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ["@" + s for s in cmudict.valid_symbols]
 _pinyin = ["@" + s for s in pinyin.valid_symbols]
-_pinyin2 = [s for s in pinyin.valid_symbols2]
 
+_pinyin4 = [s for s in pinyin.valid_symbols4] #  [ENGLISH]
+_pinyin3 = [s for s in pinyin.valid_symbols3] #  [GERMAN]
+_pinyin2 = [s for s in pinyin.valid_symbols2] #  [GERMAN]
+
+_pinyin2_b = [s for s in pinyin.valid_symbols2_b]
+
+### T1 [GERMAN]
+# Export all symbols:
+# symbols = (
+#     [_pad]
+#     + list(_special)
+#     + list(_punctuation)
+#     + list(_letters)
+#     + _arpabet
+#     + _pinyin
+#     + _pinyin2_b
+#     # + list(_letters_ipa) ## added @ pinyin.py or not
+#     + _silences
+# )
+
+### T2 [GERMAN]
+# Export all symbols:
+# symbols = (
+#     [_pad]
+#     + list(_special)
+#     + list(_punctuation)
+#     + list(_letters)
+#     + _arpabet
+#     + _pinyin
+#     + _pinyin2
+#     # + list(_letters_ipa) ## added @ pinyin.py or not
+#     + _silences
+# )
+
+### T3 [GERMAN]
 # Export all symbols:
 symbols = (
     [_pad]
@@ -29,7 +63,22 @@ symbols = (
     + list(_letters)
     # + _arpabet
     # + _pinyin
-    + _pinyin2
+    + _pinyin3
     # + list(_letters_ipa) ## added @ pinyin.py or not
     + _silences
 )
+
+
+### T4 [ENGLISH]
+# Export all symbols:
+# symbols = (
+#     [_pad]
+#     + list(_special)
+#     + list(_punctuation)
+#     + list(_letters)
+#     # + _arpabet
+#     # + _pinyin
+#     + _pinyin4
+#     # + list(_letters_ipa) ## added @ pinyin.py or not
+#     + _silences
+# )
