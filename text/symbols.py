b409ccf88fa272899ee40bc8885d27beca0f3770
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-10-03 08:21:14 +0000

    Update symbols.py
    Updated symbols along with Phones

--- text/symbols.py
+++ text/symbols.py
@@ -8,25 +8,27 @@ The default is a set of ASCII characters that works well for English or text tha
 from text import cmudict, pinyin
 
 _pad = "_"
-_punctuation = "!'(),.:;? "
+_punctuation = "!'(),.:;? " ## 10
 _special = "-"
-_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-### added @ pinyin.py or not
+_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" ## 52
+### added @ pinyin.py or nots
 # _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 
 _silences = ["@sp", "@spn", "@sil"]
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-_arpabet = ["@" + s for s in cmudict.valid_symbols]
-_pinyin = ["@" + s for s in pinyin.valid_symbols]
+_arpabet = ["@" + s for s in cmudict.valid_symbols] ## 84
+_pinyin = ["@" + s for s in pinyin.valid_symbols] ## 209
 
+_pinyin6 = [s for s in pinyin.valid_symbols6] #  [ENGLISH]
+_pinyin5 = [s for s in pinyin.valid_symbols5] #  [FRENCH]
 _pinyin4 = [s for s in pinyin.valid_symbols4] #  [ENGLISH]
 _pinyin3 = [s for s in pinyin.valid_symbols3] #  [GERMAN]
 _pinyin2 = [s for s in pinyin.valid_symbols2] #  [GERMAN]
 
 _pinyin2_b = [s for s in pinyin.valid_symbols2_b]
 
-### T1 [GERMAN]
+### T1
 # Export all symbols:
 # symbols = (
 #     [_pad]
@@ -40,7 +42,7 @@ _pinyin2_b = [s for s in pinyin.valid_symbols2_b]
 #     + _silences
 # )
 
-### T2 [GERMAN]
+### T2
 # Export all symbols:
 # symbols = (
 #     [_pad]
@@ -54,22 +56,7 @@ _pinyin2_b = [s for s in pinyin.valid_symbols2_b]
 #     + _silences
 # )
 
-### T3 [GERMAN]
-# Export all symbols:
-symbols = (
-    [_pad]
-    + list(_special)
-    + list(_punctuation)
-    + list(_letters)
-    # + _arpabet
-    # + _pinyin
-    + _pinyin3
-    # + list(_letters_ipa) ## added @ pinyin.py or not
-    + _silences
-)
-
-
-### T4 [ENGLISH]
+### T3
 # Export all symbols:
 # symbols = (
 #     [_pad]
@@ -78,7 +65,51 @@ symbols = (
 #     + list(_letters)
 #     # + _arpabet
 #     # + _pinyin
+#     + _pinyin3
+#     # + list(_letters_ipa) ## added @ pinyin.py or not
+#     + _silences
+# )
+
+### T4_MR
+# Export all symbols:
+# symbols = (
+#     [_pad]
+#     + list(_special)
+#     + list(_punctuation)
+#     # + list(_letters)
+#     # + _arpabet
+#     # + _pinyin
 #     + _pinyin4
 #     # + list(_letters_ipa) ## added @ pinyin.py or not
 #     + _silences
 # )
+
+
+### T5 [FRENCH]
+# Export all symbols:
+# symbols = (
+#     [_pad]
+#     + list(_special)
+#     + list(_punctuation)
+#     + list(_letters)
+#     # + _arpabet
+#     # + _pinyin
+#     + _pinyin5
+#     # + list(_letters_ipa) ## added @ pinyin.py or not
+#     + _silences
+# )
+
+
+### T6 [ENGLISH]
+# Export all symbols:
+symbols = (
+    [_pad]
+    + list(_special)
+    + list(_punctuation)
+    # + list(_letters)
+    # + _arpabet
+    # + _pinyin
+    + _pinyin6
+    # + list(_letters_ipa) ## added @ pinyin.py or not
+    + _silences
+)
