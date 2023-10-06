1ea2d43f7d81c767102b38f0e557bb7163fdb38b
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-10-06 05:06:32 +0000

    Update loss.py
    Added Another Codes for Matching Shape when Frame Level

--- model/loss.py
+++ model/loss.py
@@ -52,6 +52,8 @@ class FastSpeech2Loss(nn.Module):
         elif self.pitch_feature_level == "frame_level":
             pitch_predictions = pitch_predictions[:, :mel_masks.shape[1]] ## Added for matching the shape
             pitch_predictions = pitch_predictions.masked_select(mel_masks)
+
+            pitch_targets = pitch_targets[:, :mel_masks.shape[1]] ## Added for matching the shape
             pitch_targets = pitch_targets.masked_select(mel_masks)
 
         if self.energy_feature_level == "phoneme_level":
@@ -62,6 +64,8 @@ class FastSpeech2Loss(nn.Module):
         if self.energy_feature_level == "frame_level":
             energy_predictions = energy_predictions[:, :mel_masks.shape[1]] ## Added for matching the shape
             energy_predictions = energy_predictions.masked_select(mel_masks)
+            
+            energy_targets = energy_targets[:, :mel_masks.shape[1]] ## Added for matching the shape
             energy_targets = energy_targets.masked_select(mel_masks)
 
         log_duration_predictions = log_duration_predictions.masked_select(src_masks)
