ab01a066407546c6ba427ea1d8590a4f52a74e95
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-11-11 16:29:42 +0900

    Update modules.py
    changed the codes for applying parameters to each predictor

--- model/modules.py
+++ model/modules.py
@@ -26,18 +26,17 @@ from utils.tools import get_mask_from_lengths, pad
 
 
 
-########################################## 08.21(Mon) ################################################
-######## Need Modify code of `text` and add `pinyin`
+######################################## T4MR_15, T4MR_16 ##################################################
 class VarianceAdaptor(nn.Module):
     """Variance Adaptor"""
 
     def __init__(self, preprocess_config, model_config, device):
         super(VarianceAdaptor, self).__init__()
         self.device = device
-        self.duration_predictor = VariancePredictor(model_config)
         self.length_regulator = LengthRegulator(device = self.device)
-        self.pitch_predictor = VariancePredictor(model_config)
-        self.energy_predictor = VariancePredictor(model_config)
+        self.duration_predictor = VariancePredictor(model_config, predictor = 'duration') ## original: VariancePredictor(model_config)
+        self.pitch_predictor = VariancePredictor(model_config, predictor = 'pitch')       ## original: VariancePredictor(model_config)
+        self.energy_predictor = VariancePredictor(model_config, predictor = 'energy')     ## original: VariancePredictor(model_config)
 
         self.pitch_feature_level = preprocess_config["preprocessing"]["pitch"]["feature"] # 'phoneme_level'
         self.energy_feature_level = preprocess_config["preprocessing"]["energy"]["feature"] # 'phoneme_level'
@@ -112,7 +111,10 @@ class VarianceAdaptor(nn.Module):
     ## src_masks: [16, 214]
     ## max_mel_len: 1465
 
+        ## Duration Predictor
         log_duration_prediction = self.duration_predictor(x, src_mask)
+
+        ## Pitch Predictor
         if self.pitch_feature_level == "phoneme_level":
             ## p_targets: [16, 214]
             pitch_prediction, pitch_embedding = self.get_pitch_embedding(x, pitch_target, src_mask, p_control)
@@ -120,7 +122,8 @@ class VarianceAdaptor(nn.Module):
             ## pitch_embedding: [16, 214, 256]
             # x = x + pitch_embedding
             # x: [16, 214, 256]
-        
+
+        ## Energy Predictor
         if self.energy_feature_level == "phoneme_level":
             ## e_targets: [16, 214]
             energy_prediction, energy_embedding = self.get_energy_embedding(x, energy_target, src_mask, p_control)
@@ -129,31 +132,36 @@ class VarianceAdaptor(nn.Module):
             # x = x + energy_embedding
             # x: [16, 214, 256]
 
-        ## HGU-DLLAB methods
-        x = x +  pitch_embedding + energy_embedding
-        
+        ### Pitch & Energy: Phoneme-level (default)
+        ### HGU-DLLab
+        x = x + pitch_embedding + energy_embedding
+        # x: [16, 214, 256]
+
+        ## Length Regulator
         if duration_target is not None:
-            # duration_target: [16, 214]
+            ### duration_target: [16, 214]
             x, mel_len = self.length_regulator(x, duration_target, max_len)
-            # x: [16, 1465, 256]
-            # mel_len: 16
+            ### x: [16, 1465, 256]
+            ### mel_len: 16
             duration_rounded = duration_target
-            # duration_rounded = duration_target: [16, 214]
+            ### duration_rounded = duration_target: [16, 214]
         else:
             duration_rounded = torch.clamp((torch.round(torch.exp(log_duration_prediction) - 1) * d_control), min=0,)
             x, mel_len = self.length_regulator(x, duration_rounded, max_len)
-            # mel_mask = get_mask_from_lengths(mel_len.to(device)) ## cuda:0 and cpu Error! -> to(device)
-            mel_mask = get_mask_from_lengths(mel_len)
-
+            ### mel_mask = get_mask_from_lengths(mel_len.to(device)) # Before Modified - Error; CUDA 0 and CPU
+            mel_mask = get_mask_from_lengths(mel_len) # Modified @ utils.tools.py
+            
         ### frame level!: we dont care 
         if self.pitch_feature_level == "frame_level":
             pitch_prediction, pitch_embedding = self.get_pitch_embedding(x, pitch_target, mel_mask, p_control)
             x = x + pitch_embedding
         if self.energy_feature_level == "frame_level":
             energy_prediction, energy_embedding = self.get_energy_embedding(x, energy_target, mel_mask, p_control)
-            x = x + energy_embedding
+            # x = x + energy_embedding
+        if self.energy_feature_level == "frame_level" and self.pitch_feature_level == "frame_level":
+            x = x + pitch_embedding + energy_embedding
+            # x: [16, 214, 256]
 
-        
         return (
             x,                       # [16, 1465, 256]
             pitch_prediction,        # [16, 214]
@@ -163,37 +171,54 @@ class VarianceAdaptor(nn.Module):
             mel_len,                 # 1465
             mel_mask,                # [16, 1465]
         )
+##########################################################################################
 
-########################################## 08.21(Mon) ################################################
+
+###################################### T4MR16 ####################################################
 class VariancePredictor(nn.Module):
     """Duration, Pitch and Energy Predictor"""
 
-    def __init__(self, model_config):
+    def __init__(self, model_config, predictor = 'duration'):
         super(VariancePredictor, self).__init__()
 
         self.input_size = model_config["transformer"]["encoder_hidden"] # 256
-        self.filter_size = model_config["variance_predictor"]["filter_size"] # 256
-        self.kernel = model_config["variance_predictor"]["kernel_size"] # 3
-        self.conv_output_size = model_config["variance_predictor"]["filter_size"] #256
-        self.dropout = model_config["variance_predictor"]["dropout"] # .5
 
-        self.conv_layer = nn.Sequential(OrderedDict([
-            ### 1
-            ("conv1d_1", Conv(self.input_size, self.filter_size, kernel_size = self.kernel, padding=(self.kernel - 1) // 2),),
-            ("relu_1", nn.ReLU()),
-            ("layer_norm_1", nn.LayerNorm(self.filter_size)),
-            ("dropout_1", nn.Dropout(self.dropout)),
-            #### 2
-            ("conv1d_2", Conv(self.filter_size, self.filter_size, kernel_size = self.kernel, padding=1,),
-            ("relu_2", nn.ReLU()),
-            ("layer_norm_2", nn.LayerNorm(self.filter_size)),
-            ("dropout_2", nn.Dropout(self.dropout)),
-                            ]))
+        if predictor == 'duration':
+            self.kernel = model_config["variance_predictor"]['duration_predictor']["kernel_size"] # 3
+            self.filter_size = model_config["variance_predictor"]['duration_predictor']["filter_size"] # 256
+            self.conv_output_size = model_config["variance_predictor"]['duration_predictor']["filter_size"] # 256
+            self.dropout = model_config["variance_predictor"]['duration_predictor']["dropout"]   # .5
+            self.n_layers = model_config["variance_predictor"]['duration_predictor']["n_layers"] # default: 2
 
+        elif predictor == 'pitch':
+            self.kernel = model_config["variance_predictor"]['pitch_predictor']["kernel_size"] # 3
+            self.filter_size = model_config["variance_predictor"]['pitch_predictor']["filter_size"] # 256
+            self.conv_output_size = model_config["variance_predictor"]['pitch_predictor']["filter_size"] # 256
+            self.dropout = model_config["variance_predictor"]['pitch_predictor']["dropout"]   # .5
+            self.n_layers = model_config["variance_predictor"]['pitch_predictor']["n_layers"] # default: 2
+        
+        elif predictor == 'energy':
+            self.kernel = model_config["variance_predictor"]['energy_predictor']["kernel_size"] # 3
+            self.filter_size = model_config["variance_predictor"]['energy_predictor']["filter_size"] # 256
+            self.conv_output_size = model_config["variance_predictor"]['energy_predictor']["filter_size"] #256
+            self.dropout = model_config["variance_predictor"]['energy_predictor']["dropout"]   # .5
+            self.n_layers = model_config["variance_predictor"]['energy_predictor']["n_layers"] # default: 2
+
+        self.conv_set_layer = nn.Sequential(
+            Conv(self.input_size, self.filter_size, kernel_size = self.kernel, padding=(self.kernel - 1) // 2),
+            nn.ReLU(),
+            nn.LayerNorm(self.filter_size),
+            nn.Dropout(self.dropout),
+            )
+
+        self.conv_layers = nn.ModuleList([copy.deepcopy(self.conv_set_layer) for _ in range(self.n_layers)])
         self.linear_layer = nn.Linear(self.conv_output_size, 1)
 
     def forward(self, encoder_output, mask):
-        out = self.conv_layer(encoder_output)
+        out = encoder_output
+        for layer in self.conv_layers:
+            out = layer(out)
+
         out = self.linear_layer(out)
         out = out.squeeze(-1)
 
@@ -201,6 +226,52 @@ class VariancePredictor(nn.Module):
             out = out.masked_fill(mask, 0.0)
 
         return out
+##########################################################################################
+
+
+    
+##########################################################################################
+class Conv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=True, w_init="linear",):
+        """
+        :param in_channels: dimension of input
+        :param out_channels: dimension of output
+        :param kernel_size: size of kernel
+        :param stride: size of stride
+        :param padding: size of padding
+        :param dilation: dilation rate
+        :param bias: boolean. if True, bias is included.
+        :param w_init: str. weight inits with xavier initialization.
+        """
+        super(Conv, self).__init__()
+
+        self.conv = nn.Conv1d(
+            in_channels,             ## input_size = 256
+            out_channels,            ## filter_size = 256 
+            kernel_size=kernel_size, ## kernel = 3
+            stride=stride,           ## 1
+            padding=padding,         ## 0 but (self.kernel(=3) - 1) // 2 --> 1
+            dilation=dilation,       ## 1
+            bias=bias,               ## True
+        )
+
+    def forward(self, x):
+        # x: enc_output
+        # x: [Batch Size, Max_Mel_Length, d_model] [16, 1404, 256])
+        # print(x.shape)
+        x = x.contiguous().transpose(1, 2)
+        # x: --> [Batch Size, d_model, Max_Mel_Length,] [16, 256, 1404]
+        # print(x.shape)
+        x = self.conv(x)
+        # x: --> [Batch Size, d_model, Max_Mel_Length,] [16, 256, 1404]
+        # print(x.shape)
+        x = x.contiguous().transpose(1, 2)
+        # print(x.shape)
+        # x: --> [Batch Size, Max_Mel_Length, d_model] [16, 1404, 256]) 
+
+        return x
+
+
     
 
 ########################################## 08.21(Mon) ################################################
